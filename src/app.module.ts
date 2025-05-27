351d4ebfd20d6a2bab2759637099a20ba1d0f06d
Sheryar <alisharyar93@gmail.com>
2025-05-27 22:58:44 +0500

    `Added @nestjs/throttler module and ThrottlerGuard to prevent excessive requests`

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,29 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, UserModule],
+  imports: [UsersModule, 
+            DatabaseModule, 
+            UserModule,
+            ThrottlerModule.forRoot([{
+              name: 'short',
+              ttl: 60000,
+              limit: 3
+            },{
+              name: 'long',
+              ttl: 60000,
+              limit: 100
+            }
+          ]) //3 reqs per minute
+
+          ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard,
+  }],
 })
 export class AppModule {}
